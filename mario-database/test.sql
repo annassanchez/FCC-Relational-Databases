97199c6d535766b306e45f6a0b13a831afe27032
annassanchez <annassanchez@gmail.com>
2023-05-22 14:53:37 +0200

    adding changes to sql file

--- mario-database/test.sql
+++ mario-database/test.sql
@@ -56,3 +56,58 @@ INSERT INTO second_table(id, username) VALUES(2,'Mario');
 SELECT * FROM second_table;
 -- 032 use INSERT INTO to add more values to second_table
 INSERT INTO second_table(id, username) VALUES(3,'Luigi');
+-- 033 use SELECT to display the new values added to the table
+SELECT * FROM second_table;
+-- 034 use DELETE FROM table_name WHERE condition; to delete rows from a table given a condition
+DELETE FROM second_table WHERE username='Luigi';
+-- 035 use SELECT to display the table
+select * FROM second_table;
+-- 036 use DELETE FROM to delete the row by a given condition
+DELETE FROM second_table WHERE username='Mario';
+-- 037 use DELETE FROM to delete a row by a given condition
+DELETE FROM second_table WHERE username='Samus';
+-- 038 SELECT columns from table to diplay if the rows have been deleted
+SELECT * FROM second_table;
+-- 039 use \d to display the details from second_table and see if the columns where deleted
+\d second_table
+-- 040 use ALTER TABLE to drop columns
+ALTER TABLE second_table DROP COLUMN username;
+ALTER TABLE second_table DROP COLUMN name;
+-- 041 use ALTER TABLE to drop the id column
+ALTER TABLE second_table DROP COLUMN id;
+-- 042 display this database to see the changes
+\d
+-- 043 use DROP TABLE table_name to drop second_table from your database
+DROP TABLE second_table;
+-- 044 use DROP TABLE to drop first_table
+DROP TABLE first_table;
+-- 045 use \l to list all databases
+\l
+-- 046 use ALTER DATABASE database_name RENAME TO new_database_name to rename the database to mario_database
+ALTER DATABASE first_database RENAME TO mario_database;
+-- 047 use \l to list all databases
+\l
+-- 048 connect to the newly created database
+\c mario_database
+-- 049 use DROP DATABASE to remove second_database
+DROP DATABASE second_database;
+-- 050 list all elements on the databases to make sure is done
+\l
+-- 051 display the changes to make sure
+\d
+-- 052 create new table with CREATE TABLE
+CREATE TABLE characters();
+-- 053 use ALTER TABLE to add column to the characters table
+ALTER TABLE characters ADD COLUMN character_id SERIAL;
+-- 054 use \d to display the newly created columns to characters table
+\d characters
+-- 055 use ALTER TABLE to add new columns to characters table
+ALTER TABLE characters ADD COLUMN name VARCHAR(30) NOT NULL;
+-- 056 use ALTER TABLE to add new columns to characters table
+ALTER TABLE characters ADD COLUMN homeland VARCHAR(60);
+-- 057 use ALTER TABLE to add new columns to characters table
+ALTER TABLE characters ADD COLUMN favorite_color VARCHAR(30);
+-- 058 use \d to display the details from the charactes table
+\d characters
+-- 059 use INSERT INTO to add new values to characters table
+INSERT INTO characters(name, homeland, favorite_color) VALUES('Mario', 'Mushroom Kingdom', 'Red');
