3425ba455588a0f14892df1bad283d4545f224b7
annassanchez <annassanchez@gmail.com>
2025-02-19 16:52:05 +0100

    added new if statement

--- five-programs/countdown.sh
+++ five-programs/countdown.sh
@@ -2,7 +2,13 @@
 #Program that counts down to zero from a given argument
 #echo $*
 #echo $1
+#if [[ $1 == arg1 ]]
+#then
+#  echo true
+#fi
 if [[ $1 == arg1 ]]
 then
   echo true
+else
+    echo false
 fi
