a168ec8d2c3c507119250d6328d205cae71a5de9
annassanchez <annassanchez@gmail.com>
2025-02-20 16:26:31 +0100

    new if statement possibility

--- five-programs/countdown.sh
+++ five-programs/countdown.sh
@@ -24,9 +24,15 @@
 #else
 #    echo false
 #fi
+# if [[ $1 -gt 0 ]]
+# then
+#   echo true
+# else
+#     echo false
+# fi
 if [[ $1 -gt 0 ]]
 then
   echo true
 else
-    echo false
+  echo "Include a positive integer as the first argument."
 fi
