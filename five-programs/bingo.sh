8ef4cd391b9e47a75021acda3cfbe6e3e6dfc819
annassanchez <annassanchez@gmail.com>
2025-02-21 14:11:16 +0100

    git added another final else clause

--- five-programs/bingo.sh
+++ five-programs/bingo.sh
@@ -46,6 +46,22 @@
 # then
 #   echo $TEXT N:$NUMBER
 # fi
+# echo -e "\n~~ Bingo Number Generator ~~\n"
+# NUMBER=$(( RANDOM % 75 + 1 ))
+# TEXT="The next number is, "
+# if (( NUMBER <= 15 ))
+# then
+#   echo $TEXT B:$NUMBER
+# elif [[ $NUMBER -le 30 ]]
+# then
+#   echo $TEXT I:$NUMBER
+# elif (( NUMBER < 46 ))
+# then
+#   echo $TEXT N:$NUMBER
+#   elif [[ $NUMBER -lt 61 ]]
+# then
+#   echo $TEXT G:$NUMBER
+# fi
 echo -e "\n~~ Bingo Number Generator ~~\n"
 NUMBER=$(( RANDOM % 75 + 1 ))
 TEXT="The next number is, "
@@ -61,4 +77,6 @@ then
   elif [[ $NUMBER -lt 61 ]]
 then
   echo $TEXT G:$NUMBER
+else
+    echo $TEXT O:$NUMBER
 fi
