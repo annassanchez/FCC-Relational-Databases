17a0d109a79516be75f4510bb47095930b20a937
annassanchez <annassanchez@gmail.com>
2025-02-21 13:51:33 +0100

    added random numer until 75

--- five-programs/bingo.sh
+++ five-programs/bingo.sh
@@ -6,6 +6,9 @@
 # echo -e "\n~~ Bingo Number Generator ~~\n"
 # NUMBER=$RANDOM
 # echo $NUMBER
+# echo -e "\n~~ Bingo Number Generator ~~\n"
+# NUMBER=$RANDOM%75
+# echo $NUMBER
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=$RANDOM%75
+NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
