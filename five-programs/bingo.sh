a0ac5ae50af05bea6848d915fbc56b9f9b58531d
annassanchez <annassanchez@gmail.com>
2025-02-21 13:56:18 +0100

    added text

--- five-programs/bingo.sh
+++ five-programs/bingo.sh
@@ -9,6 +9,10 @@
 # echo -e "\n~~ Bingo Number Generator ~~\n"
 # NUMBER=$RANDOM%75
 # echo $NUMBER
+# echo -e "\n~~ Bingo Number Generator ~~\n"
+# NUMBER=$(( RANDOM % 75 + 1 ))
+# echo $NUMBER
 echo -e "\n~~ Bingo Number Generator ~~\n"
 NUMBER=$(( RANDOM % 75 + 1 ))
+TEXT="The next number is, "
 echo $NUMBER
