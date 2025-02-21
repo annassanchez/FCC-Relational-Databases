23ddd2424a7cc402c2d9303a041dc272b56c94ae
annassanchez <annassanchez@gmail.com>
2025-02-21 13:25:47 +0100

    added random numbers to program

--- five-programs/bingo.sh
+++ five-programs/bingo.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #Bingo Number Generator
+# echo -e "\n~~ Bingo Number Generator ~~\n"
+# NUMBER=5
+# echo $NUMBER
+# echo -e "\n~~ Bingo Number Generator ~~\n"
+# NUMBER=$RANDOM
+# echo $NUMBER
 echo -e "\n~~ Bingo Number Generator ~~\n"
+NUMBER=$RANDOM%75
+echo $NUMBER
