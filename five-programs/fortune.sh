6a7960801c70a13f8974c01617d273792ebd092f
annassanchez <annassanchez@gmail.com>
2025-02-22 12:27:02 +0100

    added else clause

--- five-programs/fortune.sh
+++ five-programs/fortune.sh
@@ -57,12 +57,33 @@
 # RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 # N=$(( RANDOM % 6 ))
 # echo ${RESPONSES[$N]}
+# echo -e "\n~~ Fortune Teller ~~\n"
+
+# GET_FORTUNE() {
+#     if [[ ! $1 ]]
+#     then
+#         echo Ask a yes or no question:
+#     fi
+
+#     read QUESTION
+# }
+
+# until [[ $QUESTION =~ \?$ ]]
+# do
+#   GET_FORTUNE
+# done
+
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
 echo -e "\n~~ Fortune Teller ~~\n"
 
 GET_FORTUNE() {
     if [[ ! $1 ]]
     then
         echo Ask a yes or no question:
+    else
+        echo Try again. Make sure it ends with a question mark:
     fi
 
     read QUESTION
