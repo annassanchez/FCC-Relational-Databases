a436b94cfb2588cf49c803b587150da6695462c5
annassanchez <annassanchez@gmail.com>
2025-02-22 11:13:47 +0100

    added scripting

--- five-programs/fortune.sh
+++ five-programs/fortune.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #Program to tell a persons fortune
+# echo -e "\n~~ Fortune Teller ~~\n"
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# echo ${RESPONSES[5]}
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
+# GET_FORTUNE() {}
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 echo ${RESPONSES[5]}
 N=$(( RANDOM % 6 ))
 echo ${RESPONSES[$N]}
-GET_FORTUNE() {}
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+}
