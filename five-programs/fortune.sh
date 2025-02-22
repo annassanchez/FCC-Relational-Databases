c9758b25e799aee99b4d2cdd7104b367e596d692
annassanchez <annassanchez@gmail.com>
2025-02-22 12:33:21 +0100

    making trhe echo rsponse a bit cleaner

--- five-programs/fortune.sh
+++ five-programs/fortune.sh
@@ -118,6 +118,29 @@
 # RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 # N=$(( RANDOM % 6 ))
 # echo ${RESPONSES[$N]}
+# echo -e "\n~~ Fortune Teller ~~\n"
+
+# GET_FORTUNE() {
+#     if [[ ! $1 ]]
+#     then
+#         echo Ask a yes or no question:
+#     else
+#         echo Try again. Make sure it ends with a question mark:
+#     fi
+
+#     read QUESTION
+# }
+
+# GET_FORTUNE
+
+# until [[ $QUESTION =~ \?$ ]]
+# do
+#   GET_FORTUNE again
+# done
+
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
 echo -e "\n~~ Fortune Teller ~~\n"
 
 GET_FORTUNE() {
@@ -140,4 +163,4 @@ done
 
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
-echo ${RESPONSES[$N]}
+echo -e "\n${RESPONSES[$N]}"
