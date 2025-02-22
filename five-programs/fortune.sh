c508b8251d62d7e1ba9d12fb36bbd76f5adb1e30
annassanchez <annassanchez@gmail.com>
2025-02-22 12:21:12 +0100

    polish the script

--- five-programs/fortune.sh
+++ five-programs/fortune.sh
@@ -6,11 +6,54 @@
 # N=$(( RANDOM % 6 ))
 # echo ${RESPONSES[$N]}
 # GET_FORTUNE() {}
+# echo -e "\n~~ Fortune Teller ~~\n"
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# echo ${RESPONSES[5]}
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
+# GET_FORTUNE() {
+#   echo Ask a yes or no question:
+# }
+# GET_FORTUNE
+# echo -e "\n~~ Fortune Teller ~~\n"
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# echo ${RESPONSES[5]}
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
+# GET_FORTUNE() {
+#   echo Ask a yes or no question:
+# }
+# GET_FORTUNE
+# read QUESTION
+# echo -e "\n~~ Fortune Teller ~~\n"
+
+# GET_FORTUNE() {
+#   echo "Ask a yes or no question:"
+# }
+
+# read QUESTION
+# until [[ $QUESTION == "test?" ]]
+# do
+#   echo $QUESTION
+#   GET_FORTUNE
+  
+# done
+
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
 echo -e "\n~~ Fortune Teller ~~\n"
+
+GET_FORTUNE() {
+  echo "Ask a yes or no question:"
+}
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+  read QUESTION
+done
+
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-echo ${RESPONSES[5]}
 N=$(( RANDOM % 6 ))
 echo ${RESPONSES[$N]}
-GET_FORTUNE() {
-  echo Ask a yes or no question:
-}
