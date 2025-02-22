4d5e1df2f35f3654bf8d978d280beb3f7313ffae
annassanchez <annassanchez@gmail.com>
2025-02-22 12:31:00 +0100

    added new function so it executes it first

--- five-programs/fortune.sh
+++ five-programs/fortune.sh
@@ -97,6 +97,27 @@
 # RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 # N=$(( RANDOM % 6 ))
 # echo ${RESPONSES[$N]}
+# echo -e "\n~~ Fortune Teller ~~\n"
+
+# GET_FORTUNE() {
+#     if [[ ! $1 ]]
+#     then
+#         echo Ask a yes or no question:
+#     else
+#         echo Try again. Make sure it ends with a question mark:
+#     fi
+
+#     read QUESTION
+# }
+
+# until [[ $QUESTION =~ \?$ ]]
+# do
+#   GET_FORTUNE again
+# done
+
+# RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# N=$(( RANDOM % 6 ))
+# echo ${RESPONSES[$N]}
 echo -e "\n~~ Fortune Teller ~~\n"
 
 GET_FORTUNE() {
@@ -109,6 +130,8 @@ GET_FORTUNE() {
 
     read QUESTION
 }
+
+GET_FORTUNE
 
 until [[ $QUESTION =~ \?$ ]]
 do
