c1c73849acba6d8c5704ab1361288e1496d64c53
annassanchez <annassanchez@gmail.com>
2025-02-24 12:39:19 +0100

    added code for creating the four sql tables

--- student-database/student-db.sh
+++ student-database/student-db.sh
@@ -8,3 +8,15 @@ psql --username=freecodecamp --dbname=postgres
 \l
 #4. All the info from the CSV files will go into a single database. Create a new database named students.
 CREATE DATABASE students;
+#5. View the databases again to make sure it got created.
+\l
+#6. There it is. Connect to your new database so you can start adding tables.
+\c students;
+#7. The CSV files have a bunch of students with info about them, and some courses and majors. You will have four tables. One for the students and their info, one for each major, another for each course, and a final one for showing what courses are included in each major. First, create the students table.
+CREATE TABLE students();
+#8. The second table will be for each unique major that appears in the data. Create a table named majors.
+CREATE TABLE majors();
+#9. The third table is for each unique course in the data. Create another table named courses.
+CREATE TABLE courses();
+#10. The final table will be a junction table for the majors and courses. Create it with the name majors_courses.
+CREATE TABLE majors_courses();
