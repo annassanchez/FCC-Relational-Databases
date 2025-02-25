b966898c4d08ef769b4eb43c79571fbf5410dacf
annassanchez <annassanchez@gmail.com>
2025-02-25 12:13:02 +0100

    added code for truncate

--- student-database/student-db.sh
+++ student-database/student-db.sh
@@ -692,3 +692,50 @@ then
     # insert into majors_courses
     fi
 done
+#97. In the psql prompt, truncate the data from the majors table so you can run the script again.
+TRUNCATE majors, students, majors_courses;
+#98. Run the script to see if the courses get inserted into the database.
+./insert_data.sh
+#99. It looks like it worked. The test data has three unique courses, and three got added to the database. View the data in the courses table to make sure they are correct.
+SELECT * FROM courses;
+#100. It looks like it worked. The test data has three unique courses, and three got added to the database. View the data in the courses table to make sure they are correct.
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
+cat courses_test.csv | while IFS="," read MAJOR COURSE
+do
+if [[ $MAJOR != major ]]
+then
+    # get major_id
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    # if not found
+    if [[ -z $MAJOR_ID ]]
+    then
+        # insert major
+        INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+        if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+        then
+            echo Inserted into majors, $MAJOR
+        fi
+        # get new major_id
+        MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    fi
+
+    # get course_id
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
+    # if not found
+
+    if [[ -z $COURSE_ID ]]
+    then
+        # insert course
+        INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+        if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+        then
+            echo Inserted into courses, $COURSE
+        fi
+        # get new course_id
+    fi
+
+    # insert into majors_courses
+    fi
+done
