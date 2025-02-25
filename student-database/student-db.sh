bc145d219721d03d476e90b94e2a932575cabac7
annassanchez <annassanchez@gmail.com>
2025-02-25 09:56:02 +0100

    added new line to the script

--- student-database/student-db.sh
+++ student-database/student-db.sh
@@ -501,3 +501,42 @@ then
     # insert into majors_courses
     fi
 done
+#90. In the psql prompt, truncate the majors table again so you can run the script and see the output.
+TRUNCATE majors, students, majors_courses;
+#91. Check to make sure the table is empty. Then, run the script.
+SELECT * FROM majors;
+./insert_data.sh
+#92. It's starting to come together. Below your get new major_id comment, set the MAJOR_ID variable to a query that gets the new major_id from the database.
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+cat courses_test.csv | while IFS="," read MAJOR COURSE
+do
+if [[ $MAJOR != major ]]
+then
+    # get major_id
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+    # if not found
+    if [[ -z $MAJOR_ID ]]
+    then
+        # insert major
+        INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+        if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+        then
+            echo Inserted into majors, $MAJOR
+        fi
+        # get new major_id
+        MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    fi
+
+    # get course_id
+
+    # if not found
+
+    # insert course
+
+    # get new course_id
+
+    # insert into majors_courses
+    fi
+done
