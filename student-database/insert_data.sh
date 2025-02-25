d1115fd33aa689ddef27c32eb7f51ba4130af90c
annassanchez <annassanchez@gmail.com>
2025-02-25 10:25:23 +0100

    added insert

--- student-database/insert_data.sh
+++ student-database/insert_data.sh
@@ -402,6 +402,42 @@
 #     # insert into majors_courses
 #     fi
 # done
+# PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+# cat courses_test.csv | while IFS="," read MAJOR COURSE
+# do
+# if [[ $MAJOR != major ]]
+# then
+#     # get major_id
+#     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+#     # if not found
+#     if [[ -z $MAJOR_ID ]]
+#     then
+#         # insert major
+#         INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+#         if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+#         then
+#             echo Inserted into majors, $MAJOR
+#         fi
+#         # get new major_id
+#         MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+#     fi
+
+#     # get course_id
+#     COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
+#     # if not found
+
+#     if [[ -z $COURSE_ID ]]
+#     then
+#         # insert course
+#         # get new course_id
+#     fi
+
+#     # insert into majors_courses
+#     fi
+# done
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 cat courses_test.csv | while IFS="," read MAJOR COURSE
@@ -432,6 +468,7 @@ then
     if [[ -z $COURSE_ID ]]
     then
         # insert course
+        INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
         # get new course_id
     fi
 
