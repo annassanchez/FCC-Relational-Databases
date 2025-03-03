6ce9267651cfa398cf16c667ceb8ddc474eb2254
annassanchez <annassanchez@gmail.com>
2025-03-03 09:24:55 +0100

    added files, created db and tables

---
+++ world-cup-database/world-cup-database.sh
@@ -0,0 +1,10 @@
+#1. connect to sql
+psql --username=freecodecamp --dbname=postgres
+#2. create database
+CREATE DATABASE worldcup;
+#3. connect to database
+\c worldcup;
+#4. create table teams
+CREATE TABLE teams();
+#5. create table games
+CREATE TABLE games();
---
+++ world-cup-database/world-cup-database.sql
@@ -0,0 +1,6 @@
+--1. create database
+CREATE DATABASE worldcup;
+--4. create table teams
+CREATE TABLE teams();
+--5. create table games
+CREATE TABLE games();
